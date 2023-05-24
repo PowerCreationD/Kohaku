adce86d1e4a57237d56c4db6ac5e0daee4eb764c
jozie-wang <jozie.wang@m2-labo.jp>
2023-05-24 17:10:47 +0800

    feat: Divide milestones into component

---
+++ src/components/about/milestone.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="milestone">
+    <h2 class="milestone__title">MILESTONE</h2>
+    <div class="milestone-wrap">
+      <div class="phase" v-for="item in milestone" :key="item.key">
+        <p class="phase__date">{{ item.date }}</p>
+        <div class="phase__things">
+          <p class="phase__thing" v-for="thing in item.content" :key="thing">{{ thing }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'milestoneComponent',
+  data() {
+    return {
+      milestone: [
+        {
+          date: '2021/09',
+          content: [
+            '與「株式会社エムスクエア・ラボ（m2labo）」、「株式会社やさいバス（Vegibus）」合作並獲其投資',
+            '成立創次方股份有限公司'
+          ]
+        },
+        {
+          date: '2022/04',
+          content: ['獲111年度教育部「U-start創新創業計畫」第一階段補助']
+        },
+        {
+          date: '2022/07',
+          content: ['成立琥白股份有限公司']
+        },
+        {
+          date: '2022/08',
+          content: [
+            '與「スズキ株式会社（Suzuki）」、「株式会社エムスクエア・ラボ（m2labo）」合作，共同開發MobileMover'
+          ]
+        },
+        {
+          date: '2023/03',
+          content: ['與「株式会社CuboRex」合作，取得產品代理權']
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+  font-family: 'Ubuntu', sans-serif;
+  padding: 0;
+  margin: 0;
+}
+.milestone {
+  margin-top: 76px;
+  &-wrap {
+    display: flex;
+    flex-direction: column;
+    margin-top: 164px;
+    width: min(900px, 100%);
+    margin-left: auto;
+  }
+  .phase {
+    display: flex;
+    margin-bottom: 16px;
+    &__date {
+      width: 61px;
+      margin-right: 51px;
+    }
+    &__things {
+      flex-grow: 1;
+      p + p {
+        margin-top: 16px;
+      }
+    }
+  }
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -65,17 +65,8 @@
     </div>
 
     <!-- * Milestone -->
-    <div class="milestone">
-      <h2 class="milestone__title">MILESTONE</h2>
-      <div class="milestone-wrap">
-        <div class="phase" v-for="item in milestone" :key="item.key">
-          <p class="phase__date">{{ item.date }}</p>
-          <div class="phase__things">
-            <p class="phase__thing" v-for="thing in item.content" :key="thing">{{ thing }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <milestone></milestone>
+
   </div>
 </template>
 
@@ -86,16 +77,10 @@
   padding: 0;
   margin: 0;
 }
-p {
-  font-weight: 400;
-}
-h1,h2,h3{
-  font-weight: 700;
-}
 $gray: #777;
 
 .about {
-  width: min(90%, 1200px);
+  width: min(1200px, 90%, calc(100% - 32px));
   margin: auto;
   padding: 120px 0;
 }
@@ -198,35 +183,12 @@ $gray: #777;
     }
   }
 }
-
-.milestone {
-  margin-top: 76px;
-  &-wrap {
-    display: flex;
-    flex-direction: column;
-    margin-top: 164px;
-    width: min(900px, 100%);
-    margin-left: auto;
-  }
-  .phase {
-    display: flex;
-    margin-bottom: 16px;
-    &__date {
-      width: 61px;
-      margin-right: 51px;
-    }
-    &__things {
-      flex-grow: 1;
-      p + p {
-        margin-top: 16px;
-      }
-    }
-  }
-}
 </style>
 
 <script>
+import milestone from '../components/about/milestone.vue'
 export default {
+  components: { milestone },
   data() {
     return {
       banner: {
@@ -240,34 +202,7 @@ export default {
       amber: {
         src: '/src/assets/about/image_amber.png',
         alt: 'image_amber'
-      },
-      milestone: [
-        {
-          date: '2021/09',
-          content: [
-            '與「株式会社エムスクエア・ラボ（m2labo）」、「株式会社やさいバス（Vegibus）」合作並獲其投資',
-            '成立創次方股份有限公司'
-          ]
-        },
-        {
-          date: '2022/04',
-          content: ['獲111年度教育部「U-start創新創業計畫」第一階段補助']
-        },
-        {
-          date: '2022/07',
-          content: ['成立琥白股份有限公司']
-        },
-        {
-          date: '2022/08',
-          content: [
-            '與「スズキ株式会社（Suzuki）」、「株式会社エムスクエア・ラボ（m2labo）」合作，共同開發MobileMover'
-          ]
-        },
-        {
-          date: '2023/03',
-          content: ['與「株式会社CuboRex」合作，取得產品代理權']
-        }
-      ]
+      }
     }
   }
 }
