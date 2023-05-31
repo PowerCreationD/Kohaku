44781abd122fb348f8464445f67ebba0ef90e6d3
jozie-wang <jozie.wang@m2-labo.jp>
2023-06-01 00:51:34 +0800

    feat: develop mail sending feature

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="form" ref="top">
+  <div class="form">
     <form @submit.prevent="handleSubmit">
       <div class="form__page--1" v-show="formPage == 1">
         <div
@@ -34,7 +34,7 @@
           :errorMsg="budget.msg"
         >
           <label class="form__title" for="budget">預算</label>
-          <input id="budget" type="number" v-model.number="budget.value" />
+          <input id="budget" type="text" v-model="budget.value" />
         </div>
         <div
           class="form__section form__date-wrap"
@@ -43,7 +43,7 @@
         >
           <label class="form__title" for="date">預估專案時間</label>
           <div id="date" class="form__date">
-            <input type="date" v-model="date.value.startDate" />
+            <input type="date" v-model="date.value.startDate" :max="date.value.endDate" />
             <span class="form__date--dash">~</span>
             <input type="date" v-model="date.value.endDate" :min="date.value.startDate" />
           </div>
@@ -103,7 +103,9 @@
           <button type="button" class="button button--primary" @click="formPage--, scroll()">
             上一步
           </button>
-          <button type="submit" class="button button--primary">送出表單</button>
+          <button type="submit" class="button button--primary" @click="sendEmail()">
+            送出表單
+          </button>
         </div>
       </div>
     </form>
@@ -111,6 +113,8 @@
 </template>
 
 <script>
+import emailjs from '@emailjs/browser'
+
 export default {
   name: 'formComponent',
   data() {
@@ -210,7 +214,7 @@ export default {
         }
 
         // budget
-        if (!budget.value) {
+        if (!budget.value || budget.value == '0') {
           budget.error = true
           budget.msg = '此處不可為空白'
         } else {
@@ -300,6 +304,118 @@ export default {
           document.body.getBoundingClientRect().top -
           offset
       })
+    },
+    sendEmail() {
+      let typeArr = []
+      this.selectedProjectTypes.value.map((type) => {
+        typeArr.push(type.name)
+      })
+      let types = typeArr.join('、')
+      let budget = this.budget.value
+      let date = `${this.date.value.startDate} ~ ${this.date.value.endDate}`
+      let description = this.description.value
+      let person = this.person.value
+      let company = this.company.value
+      let phone = this.phone.value
+      let email = this.email.value
+
+      const templateParams = {
+        user_name: `${person}`,
+        user_email: `${email}`,
+        message: `
+        <div style="background: #F5F5F5; padding:5%; font-family: font-family: 'Ubuntu', sans-serif;">
+            <div style="background: #FFFFFF;">
+                <div style="width: 100%; height: 8px; background: #E6AE4A;"></div>
+                <div style="padding: 6%;">
+                    <div style="width:100%; display:flex; justify-content: flex-start;">
+                        <img src="https://upload.cc/i1/2023/05/31/ZdgQ2u.png" alt="Kohaku" style="width:154px; margin-left:0;">
+                    </div>
+                    <div style="margin: 39px 0 28px 0;">
+                        <article style="font-size: 16px;">
+                            <p style="margin: 0;">感謝您！</p>
+                            <p style="margin: 0;">我們已收到您的來信，將在確認內容後儘速與您接洽！</p>
+                            <p style="margin: 0;">期待與您共同描繪屬於您的理想藍圖。</p>
+                            <br>
+                            <p style="margin: 0;">以下是您的填寫內容：</p>
+                        </article>
+                    </div>
+                    <div style="padding:28px 0; border-top: 1px solid #C4C4C4; border-bottom: 1px solid #C4C4C4;">
+                        <h3 style="margin: 0 0 16px 0; font-size:20px;">專案類型</h3>
+                        <p style="margin:0; font-size: 16px;">${types}</p>
+                    </div>
+                    <div style="padding:28px 0; border-bottom: 1px solid #C4C4C4;">
+                        <h3 style="margin: 0 0 16px 0; font-size:20px;">預算</h3>
+                        <p style="margin:0; font-size: 16px;">${budget} 元</p>
+                    </div>
+                    <div style="padding:28px 0;border-bottom: 1px solid #C4C4C4;">
+                        <h3 style="margin: 0 0 16px 0; font-size:20px;">預估專案時間</h3>
+                        <p style="margin:0; font-size: 16px;">${date}</p>
+                    </div>
+                    <div style="padding:28px 0;border-bottom: 1px solid #C4C4C4;">
+                        <h3 style="margin: 0 0 16px 0; font-size:20px;">專案內容概述</h3>
+                        <p style="margin:0; font-size: 16px; white-space: pre-line;">${description}</p>
+                    </div>
+                    <div style="padding:28px 0;border-bottom: 1px solid #C4C4C4;">
+                        <h3 style="margin: 0 0 16px 0; font-size:20px;">聯絡人</h3>
+                        <p style="margin:0; font-size: 16px;">${person}</p>
+                    </div>
+                    <div style="padding:28px 0;border-bottom: 1px solid #C4C4C4;">
+                        <h3 style="margin: 0 0 16px 0; font-size:20px;">公司 / 品牌名稱</h3>
+                        <p style="margin:0; font-size: 16px;">${company}</p>
+                    </div>
+                    <div style="padding:28px 0;border-bottom: 1px solid #C4C4C4;">
+                        <h3 style="margin: 0 0 16px 0; font-size:20px;">聯絡電話</h3>
+                        <a style="margin:0; font-size: 16px; color:#000000">${phone}</a>
+                    </div>
+                    <div style="padding:28px 0;border-bottom: 1px solid #C4C4C4;">
+                        <h3 style="margin: 0 0 16px 0; font-size:20px;">聯絡信箱</h3>
+                        <a href="${email}" style="margin:0; font-size: 16px; text-decoration: none; color: #000000;">
+                            ${email}</a>
+                    </div>
+                </div>
+                <footer style="background: #3E3A39; color:#FFFFFF; padding:48px 6%; margin-top:105px">
+                    <h1 style="font-size: 20px; margin:0 0 8px 0">琥白 KOHAKU</h1>
+                    <h2 style="font-size: 16px; margin:0 0 28px 0">RESEARCH x CONCEPT = SOLUTION</h2>
+                    <br>
+                    <p style="font-size:12px; margin:0 0 12px 0;">
+                        <span style="margin-right: 12px;">Time</span>
+                        <span>Mon~Fri / 10:00-19:00</span>
+                    </p>
+                    <p style="font-size:12px; margin:0 0 12px 0;"">
+                          <span style=" margin-right: 12px;">Address</span>
+                        <a style="color: #FFFFFF;">高雄市鹽埕區大勇路11號3樓507室</a>
+                    </p>
+                    <p style=" font-size:12px; margin:0;">
+                        <span style="margin-right: 12px;">Homepage</span>
+                        <a href="https://kohakulab.com"
+                            style="text-decoration: none; color: #FFFFFF;">https://kohakulab.com</a>
+                    </p>
+                </footer>
+            </div>
+        </div>
+        `
+      }
+      emailjs.send('smtpmail', 'contact_form', templateParams, 'mwaTjvTw4wztWGKUq').then(
+        (result) => {
+          console.log('SUCCESS!', result.text)
+        },
+        (error) => {
+          console.log('FAILED...', error.text)
+        }
+      )
+    }
+  },
+  watch: {
+    budget: {
+      handler(newValue) {
+        if (!newValue.value) return
+        const result = newValue.value
+          .replace(/^0\d+/, '')
+          .replace(/\D/g, '')
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        this.$nextTick(() => (this.budget.value = result))
+      },
+      deep: true
     }
   }
 }
