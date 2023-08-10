e6ee0c25d7df9f80f307705beb3f80e345af35c9
Arthur <arthur881225@gmail.com>
2023-08-11 02:10:42 +0800

    fix: WorkDetail page LayoutType1 and LayoutType3 display problem

--- src/components/work/LayoutType1.vue
+++ src/components/work/LayoutType1.vue
@@ -1,37 +1,42 @@
 <template>
-    <div class="work_detail__img-row-1" v-lazy-container="{ selector: 'img' }">
-        <img data-src="/src/assets/image/work/system_development/mobilemover/img_mobilemover_1.jpg" alt="">
+    <div class="work_detail__img-row-1">
+        <img v-lazy="img_mobilemover_1" alt="">
     </div>
     <div class="work_detail__img-row-2">
-        <div class="row-2 row-gap" v-lazy-container="{ selector: 'img' }">
-            <img data-src="/src/assets/image/work/system_development/mobilemover/img_mobilemover_2.jpg" alt="" />
+        <div class="row-2 row-gap">
+            <img v-lazy="img_mobilemover_2" alt="" />
             <p>Mobile Mover App 可為農機設定任務排程、路線、執行內容，並於執行中即時監控載具的工作情況</p>
         </div>
-        <div class="row-2" v-lazy-container="{ selector: 'img' }">
-            <img data-src="/src/assets/image/work/system_development/mobilemover/img_mobilemover_3.png" alt="" />
+        <div class="row-2">
+            <img v-lazy="img_mobilemover_3" alt="" />
             <p>考量田間操作情境，Mobile Mover 首重移動裝置 App 開發，以平板控制並以虛擬搖桿以靈活操作</p>
         </div>
     </div>
     <div class="work_detail__img-row-3">
-        <div class="row-3" v-lazy-container="{ selector: 'img' }">
-            <img class="row-3-gap" data-src="/src/assets/image/work/system_development/mobilemover/img_mobilemover_4.png" alt="" />
-            <img class="row-3-gap" data-src="/src/assets/image/work/system_development/mobilemover/img_mobilemover_5.png" alt="" />
-            <img data-src="/src/assets/image/work/system_development/mobilemover/img_mobilemover_6.png" alt="" />
+        <div class="row-3">
+            <img class="row-3-gap" v-lazy="img_mobilemover_4" alt="" />
+            <img class="row-3-gap" v-lazy="img_mobilemover_5" alt="" />
+            <img v-lazy="img_mobilemover_6" alt="" />
         </div>
         <p>針對載具的執行路線、狀態、電量、速度、位置回報，即時監控載具，使用者可依照不同任務需求設定、編輯與儲存路徑，建立標準化工作內容。App 可管理多張地圖的多條路徑，簡明化田間管理工作內容，並透過綁定、置換使 Mobile Mover 彈性規劃任務路徑</p>
     </div>
     <div class="work_detail__img-row-4">
-        <div class="row-4" v-lazy-container="{ selector: 'img' }">
-            <img class="row-4-gap" data-src="/src/assets/image/work/system_development/mobilemover/img_mobilemover_7.jpg" alt="" />
-            <img class="" data-src="/src/assets/image/work/system_development/mobilemover/img_mobilemover_8.jpg" alt="" />
+        <div class="row-4">
+            <img class="row-4-gap" v-lazy="img_mobilemover_7" alt="" />
+            <img class="" v-lazy="img_mobilemover_8" alt="" />
         </div>
     </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import img_mobilemover_1 from '@/assets/image/work/system_development/mobilemover/img_mobilemover_1.jpg'
+import img_mobilemover_2 from '@/assets/image/work/system_development/mobilemover/img_mobilemover_2.jpg'
+import img_mobilemover_3 from '@/assets/image/work/system_development/mobilemover/img_mobilemover_3.png'
+import img_mobilemover_4 from '@/assets/image/work/system_development/mobilemover/img_mobilemover_4.png'
+import img_mobilemover_5 from '@/assets/image/work/system_development/mobilemover/img_mobilemover_5.png'
+import img_mobilemover_6 from '@/assets/image/work/system_development/mobilemover/img_mobilemover_6.png'
+import img_mobilemover_7 from '@/assets/image/work/system_development/mobilemover/img_mobilemover_7.jpg'
+import img_mobilemover_8 from '@/assets/image/work/system_development/mobilemover/img_mobilemover_8.jpg'
 </script>
 
 <style lang="scss" scoped>
