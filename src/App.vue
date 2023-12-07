ce74d0735fb51b43eb1e2808ea39d9f933032835
jozie-wang <yuejin928@gmail.com>
2023-12-07 13:13:51 +0800

    feat: update work page

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,30 @@
 <script>
 import PageHeader from './components/global/PageHeader.vue'
 import PageFooter from './components/global/PageFooter.vue'
+import { reactive, onMounted, onBeforeUnmount } from 'vue'
+
+export const mobileCheckSymbol = Symbol()
 
 export default {
-  components: { PageHeader, PageFooter }
+  components: { PageHeader, PageFooter },
+  provide() {
+    const isMobile = reactive({ value: window.innerWidth < 768 })
+
+    const handleResize = () => {
+      isMobile.value = window.innerWidth < 768
+    }
+
+    onMounted(() => {
+      window.addEventListener('resize', handleResize)
+    })
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('resize', handleResize)
+    })
+
+    return {
+      [mobileCheckSymbol]: isMobile
+    }
+  }
 }
 </script>
