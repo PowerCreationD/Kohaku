827182853cb46584ef129fe846332da493d71c32
jozie-wang <yuejin928@gmail.com>
2023-12-18 18:30:32 +0800

    fix: fix safari compatibility problem

--- src/components/home/HomeAboutSection.vue
+++ src/components/home/HomeAboutSection.vue
@@ -4,31 +4,29 @@ import grassAndAmber from '@/assets/image/global/background_grass-and-amber.jpg'
 import amberImage from '@/assets/image/global/image_amber.png'
 </script>
 <template>
-  <div class="home__section">
-    <div class="about-section">
-      <div class="about-section__container words-section">
-        <p class="font-1 words-section__title">琥白</p>
-        <p class="font-1 words-section__title">Kohaku Studio</p>
-        <img class="words-section__wave-image" :src="waveImage" alt="wave" />
-        <article class="words-section__text">
-          <p class="font-7">{{ $t('about.brand.meaning') }}</p>
-        </article>
-        <div class="words-section__background">
-          <img
-            class="words-section__background-amber words-section__background-amber--left"
-            :src="amberImage"
-            alt="kohaku"
-          />
-          <img
-            class="words-section__background-amber words-section__background-amber--right"
-            :src="amberImage"
-            alt="kohaku"
-          />
-        </div>
+  <div class="home__section about-section">
+    <div class="about-section__container words-section">
+      <p class="font-1 words-section__title">琥白</p>
+      <p class="font-1 words-section__title">Kohaku Studio</p>
+      <img class="words-section__wave-image" :src="waveImage" alt="wave" />
+      <article class="words-section__text">
+        <p class="font-7">{{ $t('about.brand.meaning') }}</p>
+      </article>
+      <div class="words-section__background">
+        <img
+          class="words-section__background-amber words-section__background-amber--left"
+          :src="amberImage"
+          alt="kohaku"
+        />
+        <img
+          class="words-section__background-amber words-section__background-amber--right"
+          :src="amberImage"
+          alt="kohaku"
+        />
       </div>
-      <div class="about-section__container image-section">
-        <img class="image-section__background-image" :src="grassAndAmber" alt="kohaku" />
-      </div>
+    </div>
+    <div class="about-section__container image-section">
+      <img class="image-section__background-image" :src="grassAndAmber" alt="kohaku" />
     </div>
   </div>
 </template>
