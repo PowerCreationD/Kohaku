e18556fddadf39450770e8e040e5cb982c779e06
jozie-wang <yuejin928@gmail.com>
2024-05-08 10:43:13 +0800

    feat: update about

--- src/components/about/CompanyTable.vue
+++ src/components/about/CompanyTable.vue
@@ -1,12 +1,19 @@
 <template>
-<div class="company-table-wrap">
+  <div class="company-table-wrap">
     <div class="company-table">
       <div v-for="item in table" :key="item" class="company-table__row">
         <div class="company-table__title font-6">{{ item.title }}</div>
-        <div class="company-table__content font-7">{{ item.content }}</div>
+        <div class="company-table__content font-7" v-if="item.name !== 'related_company'">
+          {{ item.content }}
+        </div>
+        <div v-else class="company-table__related_company font-7">
+          <p v-for="company in item.content" :key="company">
+            {{ company.name }} <br class="mobile">（統編：{{ company.business_id }}）
+          </p>
+        </div>
       </div>
     </div>
-</div>
+  </div>
 </template>
 
 <style src="@/assets/scss/about/_company-table.scss" scoped></style>
@@ -14,16 +21,24 @@
 <script>
 export default {
   name: 'CompanyTable',
+  inject: ['mobileCheck'],
   data() {
     return {
       table: [
-        { title: '公司名', content: '琥白股份有限公司' },
-        { title: '營業地址', content: '高雄市鹽埕區大勇路11號3F' },
-        { title: '負責人', content: '林凱元' },
-        { title: '資本額', content: '新台幣 2,000,000 元整' },
-        { title: '統編', content: '90349136' },
-        { title: '事業內容', content: '軟硬體系統整合開發、內容企劃、商業設計' },
-        { title: '關係企業', content: '創次方股份有限公司 （統編：90521327）' }
+        { name: 'company_name', title: '公司名', content: '琥白股份有限公司' },
+        { name: 'address', title: '營業地址', content: '高雄市鹽埕區大勇路11號3F' },
+        { name: 'responsible', title: '負責人', content: '林凱元' },
+        { name: 'capital', title: '資本額', content: '新台幣 2,000,000 元整' },
+        { name: 'business_id', title: '統編', content: '90349136' },
+        { name: 'content', title: '事業內容', content: '軟硬體系統整合開發、內容企劃、商業設計' },
+        {
+          name: 'related_company',
+          title: '關係企業',
+          content: [
+            { name: '創次方股份有限公司', business_id: '90521327' },
+            { name: '萬屋休閒農場', business_id: '88769287' }
+          ]
+        }
       ]
     }
   }
